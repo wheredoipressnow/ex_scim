aaa0ce2086b5b89273b7bdc17c5a77a3c3169260
LarsO <lars.oehnenamen@gmail.com>
2025-08-30 08:48:23 +0300

    docs(ex_scim_phoenix): add module and function documentation

--- ex_scim_phoenix/test/ex_scim_phoenix_test.exs
+++ ex_scim_phoenix/test/ex_scim_phoenix_test.exs
@@ -2,7 +2,9 @@ defmodule ExScimPhoenixTest do
   use ExUnit.Case
   doctest ExScimPhoenix
 
-  test "greets the world" do
-    assert ExScimPhoenix.hello() == :world
+  test "provides version information" do
+    version = ExScimPhoenix.version()
+    assert is_binary(version)
+    assert String.length(version) > 0
   end
 end
